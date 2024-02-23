dce35e07b654c446a8f1916fd30f694dd3a9e33f
KrulYuno <giogulane.wineffgulane@gmail.com>
2024-02-24 00:06:28 +0900

    [osr_extract] figuring out the data types

---
+++ Tools/osu_replay_data_extractor/doc/trait.impl/clap_builder/derive/trait.CommandFactory.js
@@ -0,0 +1,5 @@
+(function() {var implementors = {
+"clap":[],
+"clap_builder":[],
+"osr_extract":[["impl <a class=\"trait\" href=\"clap_builder/derive/trait.CommandFactory.html\" title=\"trait clap_builder::derive::CommandFactory\">CommandFactory</a> for <a class=\"struct\" href=\"osr_extract/struct.Cli.html\" title=\"struct osr_extract::Cli\">Cli</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
